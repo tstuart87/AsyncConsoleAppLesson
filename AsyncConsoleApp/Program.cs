c196519f74cbea5afaf03ea12d5a066ef887bfaf
Trevor Stuart <stuart.trev@gmail.com>
2021-09-13 23:32:40 -0400

    initial

---
+++ AsyncConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Do you want to do this the easy way...or the much slower way?\n" +
+                "1. Easy.\n" +
+                "2. Much slower.\n");
+
+            string reply = Console.ReadLine();
+
+            Console.Clear();
+
+            ScrambledEggsSwitchCase(reply);
+
+            Console.ReadKey();
+
+            Console.Clear();
+            Main();
+        }
+
+        static void ScrambledEggsSwitchCase(string reply)
+        {
+            ScrambledEggs _eggs = new ScrambledEggs();
+            
+            switch (reply)
+            {
+                case "1":
+                    _eggs.MakeScrambledEggsAsync();
+                    break;
+                case "2":
+                    _eggs.MakeScrambledEggs();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ AsyncConsoleApp/ScrambledEggs.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleApp
+{
+    public class ScrambledEggs
+    {
+        public void MakeScrambledEggs()
+        {
+            HeatFryingPan();
+            CrackEggsInBowl();
+            WhiskEggs();
+            PourEggsIntoHeatedPan();
+            ScrambleScrambleScramble();
+        }
+
+        public async Task MakeScrambledEggsAsync()
+        {
+            var task = HeatFryingPanAsync();
+            CrackEggsInBowl();
+            WhiskEggs();
+            Console.WriteLine(await task);
+            PourEggsIntoHeatedPan();
+            ScrambleScrambleScramble();
+        }
+
+        public void HeatFryingPan()
+        {
+            Console.WriteLine("Turning the stove on now...should only take a few minutes...no need to rush.");
+            Thread.Sleep(10000);
+            Console.WriteLine("Pan is hot and ready.");
+        }
+
+        public async Task<string> HeatFryingPanAsync()
+        {
+            Console.WriteLine("Turning the stove on now....");
+            await Task.Delay(10000);
+            return "Pan is hot and ready.";
+        }
+
+        public void CrackEggsInBowl()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Cracking eggs, Chef.");
+        }
+
+        public void WhiskEggs()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Whisking eggs, Chef.");
+        }
+
+        public void PourEggsIntoHeatedPan()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Is the pan hot yet?");
+            Thread.Sleep(2000);
+            Console.WriteLine("Yeah, I turned it on before we started cracking and whisking.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Putting the eggs in the pan, Chef.");
+        }
+
+        public void ScrambleScrambleScramble()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Scramble, scramble, scramble...vOiLa!");
+        }
+    }
+}
